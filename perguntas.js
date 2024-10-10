e6311e112274fe07537cc167cf364c7d28594170
eliassirati <164449916+eliassirati@users.noreply.github.com>
2024-10-09 23:16:52 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Google?',
+    'O Google Busca é um serviço da empresa Google, em que é possível fazer pesquisas na internet sobre qualquer tipo de assunto ou conteúdo'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasília'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Conjunto das regras e procedimentos lógicos perfeitamente definidos que levam à solução de um problema em um número finito de etapas'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Tchau em ingles é bye (pronuncia-se BAI)'
+)
